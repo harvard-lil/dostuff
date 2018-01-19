e13976d4a8572225be9d5659e99a1aa642f60b0f
Jack Cushman <jcushman@law.harvard.edu>
2018-01-19 16:07:53 -0500

    Drop static dir

--- dostuff/settings.py
+++ dostuff/settings.py
@@ -116,7 +116,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    # os.path.join(PROJECT_ROOT, 'static'),
 ]
 
 # Simplified static file serving.
