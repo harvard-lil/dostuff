ee20056b4c6f4d8691c15c06b580f41229998f35
Jack Cushman <jcushman@law.harvard.edu>
2018-01-24 10:58:11 -0500

    Parse create_users integer param

--- fabfile.py
+++ fabfile.py
@@ -35,7 +35,7 @@ def create_users(count=1):
     adjectives = "adaptable	adventurous	affectionate	ambitious	amiable	compassionate	considerate	courageous courteous	diligent	empathetic	exuberant	frank	generous	gregarious	impartial	intuitive	inventive passionate	persistent	philosophical	practical	rational	reliable	resourceful	sensible	sincere sympathetic	unassuming	witty".split()
     animals = "Dog Cat Horse Chicken Fish Bear Bird Shark Snake Pig Lion Turkey Wolf Spider Duck Deer Cow  Monkey Lobster Ape Pony Eagle Dolphin Bison".split()
 
-    for i in range(count):
+    for i in range(int(count)):
         username = ("%s %s" % (random.choice(adjectives), random.choice(animals))).title()
         user = User.objects.create_user(username=username, password=get_random_string(20))
         print(user.auth_token.key)
